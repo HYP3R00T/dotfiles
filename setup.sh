42ae0ceebd81b46024ae546c67a645377d3f461c
hyperoot <rajesh@hyperoot.dev>
2025-04-15 14:06:42 +0000

    add ssh fingerprint

--- setup.sh
+++ setup.sh
@@ -2,13 +2,19 @@
 
 set -euo pipefail
 
+# Ensure GitHub SSH fingerprint is trusted
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan -t ed25519 github.com >>~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
+
 # Install chezmoi if not already installed
-if ! command -v chezmoi > /dev/null; then
+if ! command -v chezmoi >/dev/null; then
     sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:HYP3R00T/dotfiles.git
 fi
 
 # Install mise-en-place if not already installed
-if ! command -v mise > /dev/null; then
+if ! command -v mise >/dev/null; then
     curl https://mise.run | sh
     # Ensure mise is immediately available in this session
     export PATH="$HOME/.local/bin:$PATH"
