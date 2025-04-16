bd6b073f74ea5850046c140acf7d2af4b678544a
hyperoot <rajesh@hyperoot.dev>
2025-04-16 05:07:13 +0000

    add chezmoi to path

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@
 
 set -euo pipefail
 
-# Ensure GitHub SSH fingerprint is trusted
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-ssh-keyscan -t ed25519 github.com >>~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
+# # Ensure GitHub SSH fingerprint is trusted
+# mkdir -p ~/.ssh
+# chmod 700 ~/.ssh
+# ssh-keyscan -t ed25519 github.com >>~/.ssh/known_hosts
+# chmod 644 ~/.ssh/known_hosts
 
 # Install chezmoi if not already installed
 if ! command -v chezmoi >/dev/null; then
     sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/HYP3R00T/dotfiles.git
+    # Ensure chezmoi is immediately available in this session
+    export PATH="$HOME/.local/share/chezmoi:$PATH"
 fi
 
 # Install mise-en-place if not already installed
