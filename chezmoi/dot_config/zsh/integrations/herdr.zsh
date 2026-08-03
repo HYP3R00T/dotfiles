6fd8c0ee2a594a872973434c36afa7fef0105b52
Rajesh <hyperoot.tech@proton.me>
2026-08-03 16:45:44 +0530

    chore: update configs

---
+++ chezmoi/dot_config/zsh/integrations/herdr.zsh
@@ -0,0 +1,4 @@
+# Herdr completion.
+
+(( $+commands[herdr] )) || return 0
+_zsh_source_command_cache herdr-completion herdr completion zsh
---
+++ chezmoi/dot_config/zsh/integrations/labctl.zsh
@@ -0,0 +1,4 @@
+# labctl completion.
+
+(( $+commands[labctl] )) || return 0
+_zsh_source_command_cache labctl-completion labctl completion zsh
